47f46599fb6695d6d160275f2ecc649bde500676
Jaan Janno <jaan911@ut.ee>
2015-01-13 23:42:43 +0200

    Modifications to index view. #2 Button highlighting, title fields differentiated more from buttons. Modified tests.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,13 +2,14 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.twirl.api.Html;
 import views.html.*;
 
 @SuppressWarnings("unused")
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Hello world!"));
-    }
-
+	public static Result index() {
+		Html indexContent = index.render();
+		return ok(indexContent);
+	}
 }
